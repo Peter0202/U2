3635b2cca62ea11aedad6e9c875ddf2babcc468e
Petar Bakalov <496316@student.fontys.nl>
2024-04-14 14:20:54 +0200

    Adding documentation to the repo

--- Services/U2APIGateway/Program.cs
+++ Services/U2APIGateway/Program.cs
@@ -11,9 +11,7 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
