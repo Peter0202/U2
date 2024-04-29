1bff3a0631f93ef64745178c887dc8dd2d5410f7
Petar Bakalov <496316@student.fontys.nl>
2024-04-29 17:53:16 +0200

    Created docker registry

--- Services/VideoService/Controllers/WeatherForecastController.cs
+++ Services/VideoService/Controllers/WeatherForecastController.cs
@@ -24,8 +24,8 @@ namespace U2.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                TemperatureC = 69,
+                Summary = "hui"
             })
             .ToArray();
         }
