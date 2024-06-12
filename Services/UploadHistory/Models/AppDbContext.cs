86dba7295eb5018c67fe15463c6ae99647c8aebe
Petar Bakalov <496316@student.fontys.nl>
2024-06-12 13:36:21 +0200

    Database upload

---
+++ Services/UploadHistory/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UploadHistory.Models
+{
+    public class AppDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            optionBuilder.UseMySQL(configuration.GetConnectionString("UploadDatabase"));
+        }
+    }
+}
--- Services/UploadHistory/Program.cs
+++ Services/UploadHistory/Program.cs
@@ -1,13 +1,40 @@
+using UploadHistory.Models;
 var builder = WebApplication.CreateBuilder(args);
+const string allowSpecificOrigins = "dev";
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowSpecificOrigins, policy =>
+    {
+        policy.AllowAnyOrigin();
+        policy.AllowAnyHeader();
+        policy.WithOrigins("*", "http://localhost:3000").WithMethods("GET", "POST", "DELETE", "PUT");
+    });
+});
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        context.Database.EnsureCreated();
+    }
+    catch
+    {
+
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
