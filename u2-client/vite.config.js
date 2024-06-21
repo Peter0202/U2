edc6eb6775f5b95305f6f09b44c44fd5419219d6
Petar Bakalov <496316@student.fontys.nl>
2024-06-21 16:07:46 +0200

    Test https certificates

--- u2-client/vite.config.js
+++ u2-client/vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 import fs from 'fs';
 import path from 'path';
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-      host: true,
-      strictPort: true,
-      port: 3000
- }
-});
+  plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3000,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '../deploy/local/www.example.com.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../deploy/local/www.example.com.crt'))
+    }
+  }
+})
