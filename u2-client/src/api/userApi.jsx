6c429e44dfd5434e7cd4c3cc465314ea6efba6d1
Petar Bakalov <496316@student.fontys.nl>
2024-06-26 19:27:50 +0200

    smth

--- u2-client/src/api/userApi.jsx
+++ u2-client/src/api/userApi.jsx
@@ -1,43 +1,40 @@
-import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios'
 
 const BASE_URL = 'http://48.217.13.145/api/UserService';
 
-
 export const getAllUsers = async () => {
     try {
-        const res = (await axios.get(BASE_URL + `/GetAllUsers`)).data;
+        const res = (await axios.get(BASE_URL + `/GetAllUsers`, {headers:{"Roles" : localStorage.getItem("roles")}})).data;
         return res;
     }
-    catch (err) {
+    catch(err){
         console.log(err);
     }
 }
 
 export const getUserByUsername = async (username) => {
-    const roles = localStorage.getItem("roles")
-    try {
-        return (await axios.get(BASE_URL + `/GetByUsername/?username=${username}`, {headers: {"Roles": roles}})).data;
+    try{
+        return (await axios.get(BASE_URL + `/GetByUsername/?username=${username}`)).data;
     }
-    catch (err) {
+    catch(err){
         console.log(err);
     }
 }
 
-export const createUser = async (username) => {
-    try {
+export const createUser = async (username) =>{
+    try{
         return (await axios.post(BASE_URL + `/Post/?username=${username}`));
     }
-    catch (err) {
+    catch(err){
         console.log(err);
     }
 }
 
 export const deleteUser = async (id) => {
-    try {
+    try{
         return (await axios.delete(BASE_URL + `/Delete/?id=${id}`));
     }
-    catch (err) {
+    catch(err){
         console.log(err);
     }
 }
